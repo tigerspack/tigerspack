999f2ab9602b459dfe7bb1351887e029accf4205
Andrey Arkhipov <wibox@ya.ru>
2014-07-21 18:11:05 +0400

    #4 Fix less watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
-var gulp = require('gulp'); // Сообственно Gulp JS
-var jade = require('gulp-jade'); // Плагин для Jade
-var less = require('gulp-less'); // Плагин для Less
-var imagemin = require('gulp-imagemin'); // Минификация изображений
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var less = require('gulp-less');
+var imagemin = require('gulp-imagemin');
 var pngcrush = require('imagemin-pngcrush');
-var uglify = require('gulp-uglify'); // Минификация JS
-var notify = require('gulp-notify'); // Минификация JS
-var concat = require('gulp-concat'); // Склейка файлов
+var uglify = require('gulp-uglify');
+var notify = require('gulp-notify');
+var concat = require('gulp-concat');
 
-// Собираем html из Jade
+function errorLog(err){
+    gulp.src("./source")
+        .pipe(notify(err));
+}
+
 gulp.task('jade', function() {
-    gulp.src(['./source/jade/*.jade'])
+    gulp.src(['./source/*.jade'])
         .pipe(jade({
             pretty: true
         }))
@@ -19,11 +23,12 @@ gulp.task('jade', function() {
 gulp.task('less', function() {
     gulp.src(['./source/build/theme.less','./source/build/main.less','./source/blocks/**/*.less'])
         .pipe(concat('styles.less'))
-        .pipe(less())
+        .pipe(less().on('error', function(err){
+            errorLog(err);
+        }))
         .pipe(gulp.dest('./public/css'))
 });
 
-// Собираем JS
 gulp.task('js', function() {
     gulp.src(['./source/build/jquery/*.js'])
         .pipe(concat('jquery.js'))
@@ -39,7 +44,6 @@ gulp.task('js', function() {
         .pipe(gulp.dest('./public/js'))
 });
 
-// Копируем и минимизируем изображения
 gulp.task('images', function() {
     gulp.src('./source/img/*')
         .pipe(imagemin({
@@ -50,11 +54,10 @@ gulp.task('images', function() {
         .pipe(gulp.dest('./public/img'))
 });
 
-// Запуск сервера разработки gulp watch
 gulp.task('default', ['jade','less', 'js', 'images'], function() {
     gulp.src("./source")
         .pipe(notify("Done, without errors. Running watch."));
-    gulp.watch('source/jade/**/*.jade', ['jade']);
+    gulp.watch('source/**/*.jade', ['jade']);
     gulp.watch('source/**/*.less', ['less']);
     gulp.watch('source/**/*.js', ['js']);
     gulp.watch('source/img/**/*', ['images']);
