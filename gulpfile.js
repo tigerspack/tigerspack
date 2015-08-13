540328c96df711fdf4d10754823320da4f0c189f
Andrey Arkhipov <wibox@ya.ru>
2015-08-13 17:58:38 +0300

    Переделана логика сборки

--- gulpfile.js
+++ gulpfile.js
@@ -13,9 +13,16 @@ function errorLog(err){
         .pipe(notify(err));
 }
 
+var settings = {
+    less: ['./source/less/build.less','./source/blocks/**/*.less'],
+    css: './public_html/css',
+    js_src: ['./source/jquery/*.js','./source/js/*.js','./source/blocks/*.js'],
+    js_build: './public_html/js',
+    images: './public_html/img/*'
+};
+
 var source = {
     path: './source/',
-    html: './source/*.html',
     less: ['./source/less/build.less','./source/blocks/**/*.less'],
     library: './source/js/*.js',
     js: './source/blocks/*.js',
@@ -32,28 +39,20 @@ var build = {
     images: './public_html/img'
 };
 
-gulp.task('html', function() {
-    gulp.src(source.html)
-        .pipe(gulp.dest(build.path))
-});
-
 gulp.task('less', function() {
-    gulp.src(source.less)
+    gulp.src(settings.less)
         .pipe(concat('styles.less'))
         .pipe(less().on('error', function(err){
             errorLog(err);
         }))
-        .pipe(gulp.dest(build.css))
+        .pipe(gulp.dest(settings.css))
 });
 
 gulp.task('js', function() {
-    gulp.src(source.library)
-        .pipe(uglify())
-        .pipe(gulp.dest(build.js))
-    gulp.src(source.js)
+    gulp.src(settings.js_src)
         .pipe(concat('scripts.js'))
         .pipe(uglify())
-        .pipe(gulp.dest(build.js))
+        .pipe(gulp.dest(settings.js_build))
 });
 
 gulp.task('clean', function() {
@@ -61,10 +60,9 @@ gulp.task('clean', function() {
         .pipe(clean())
 });
 
-gulp.task('images', ['clean'], function() {
-    globby(source.images, function (err, paths) {
+gulp.task('imagemin', function() {
+    globby(settings.images, function (err, paths) {
         gulp.src(paths)
-            .pipe(gulp.dest(build.images))
             .pipe(imagemin({
                 progressive: true,
                 svgoPlugins: [{removeViewBox: false}],
@@ -73,11 +71,10 @@ gulp.task('images', ['clean'], function() {
     });
 });
 
-gulp.task('default', ['html', 'less', 'js', 'images'], function() {
+gulp.task('default', ['less', 'js'], function() {
     gulp.src(source.path)
         .pipe(notify("Running watch"));
-    gulp.watch(source.html, ['html']);
     gulp.watch(source.wless, ['less']);
     gulp.watch(source.wjs, ['js']);
-    gulp.watch(source.images, ['images']);
 });
+gulp.task('images', ['imagemin']);
