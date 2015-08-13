5694dddc3d0aeb23aa71ad60a045e6fa0e7d6387
Andrey Arkhipov <wibox@ya.ru>
2015-08-13 23:56:30 +0300

    Обновлено описание

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// Подключаемые модули
 var gulp = require('gulp');
 var less = require('gulp-less');
 var imagemin = require('gulp-imagemin');
@@ -6,25 +7,25 @@ var uglify = require('gulp-uglify');
 var notify = require('gulp-notify');
 var concat = require('gulp-concat');
 var globby = require('globby');
-var clean = require('gulp-clean');
 
+// Настройки сборки
+var settings = {
+    path: './source/',                                                  // Директория исходников
+    less: ['./source/less/build.less','./source/blocks/**/*.less'],     // Исходники LESS
+    css: './public_html/css',                                           // Директория в которую собирается CSS
+    js_dir: './source/js/**/*.js',                                      // Библиотеки JS
+    js_src: './source/blocks/**/*.js',                                  // Исходники JS
+    js_build: './public_html/js',                                       // Директория сборки JS
+    images: './public_html/img/*',                                      // Директория с изображениями
+    watch_less: 'source/**/**/*.less',                                  // Где отслеживаются LESS
+    watch_js: 'source/**/**/*.js'                                       // Где отслеживаются JS
+};
+// Функция вывода ошибок
 function errorLog(err){
     gulp.src(settings.path)
         .pipe(notify(err));
 }
-
-var settings = {
-    path: './source/',
-    less: ['./source/less/build.less','./source/blocks/**/*.less'],
-    css: './public_html/css',
-    js_dir: './source/js/**/*.js',
-    js_src: './source/blocks/**/*.js',
-    js_build: './public_html/js',
-    images: './public_html/img/*',
-    watch_less: 'source/**/**/*.less',
-    watch_js: 'source/**/**/*.js'
-};
-
+// Сборка LESS
 gulp.task('less', function() {
     gulp.src(settings.less)
         .pipe(concat('styles.less'))
@@ -33,7 +34,7 @@ gulp.task('less', function() {
         }))
         .pipe(gulp.dest(settings.css))
 });
-
+// Сборка JS
 gulp.task('js', function() {
     gulp.src(settings.js_src)
         .pipe(concat('scripts.js'))
@@ -47,7 +48,7 @@ gulp.task('js', function() {
         }))
         .pipe(gulp.dest(settings.js_build))
 });
-
+// Минификация изображений
 gulp.task('imagemin', function() {
     globby(settings.images, function (err, paths) {
         gulp.src(paths)
@@ -58,7 +59,7 @@ gulp.task('imagemin', function() {
             }))
     });
 });
-
+// Запуск сборки
 gulp.task('default', ['less', 'js'], function() {
     gulp.src(settings.path)
         .pipe(notify("Running watch"));
