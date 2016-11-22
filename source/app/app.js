7ba3f7691a4b87ecc104818bc9c521b9956962ed
Andrey Arkhipov <wibox@ya.ru>
2016-11-23 00:08:01 +0300

    Чистка, удаляем лишнии стили и блоки tech и footer

--- source/app/app.js
+++ source/app/app.js
@@ -3,17 +3,13 @@ import data from './data.js';
 
 import Header from './../modules/header/header';
 import Content from './../modules/content/content';
-import Footer from './../modules/footer/footer';
 import Menu from './../modules/menu/menu';
-import Tech from './../modules/tech/tech';
 import Promo from './../modules/promo/promo';
 
 new Header(data.header);
 new Content(data.content);
-new Footer(data.footer);
 new Menu(data.menu, '#header-wrap','.menu_black');
 new Promo(data.promo, '#content-wrap');
-new Tech(data.tech, '#content-wrap');
 
 $(function(){
     $('.header__login').click(function(){
--- source/app/data.js
+++ source/app/data.js
@@ -28,39 +28,12 @@ var data = {
       body: "Текст"
     }
   },
-  tech: {
-    title: "Технологии",
-    items:  [{
-        imgClass: "news__img_webpack",
-        name: "WebpackJS",
-        text: "Webpack – один из самых мощных и гибких инструментов для сборки frontend. " +
-        "Вместе с тем, он достаточно сложен, а документация оставляет желать много лучшего.",
-        url: "#news1"
-      },
-      {
-        imgClass: "news__img_babel",
-        name: "Babel.JS",
-        text: "Стандарт ES-2015 был принят в июне 2015. Пока что большинство браузеров реализуют его частично. " +
-        "Babel.JS – это транспайлер, переписывающий код на ES-2015 в код на предыдущем стандарте ES5.",
-      },
-      {
-        imgClass: "news__img_less",
-        name: "LessCSS",
-        text: "LESS — это продукт с открытым исходным кодом. " +
-        "Less — это вложенный метаязык: валидный CSS будет валидной less-программой с аналогичной семантикой.",
-        url: "#news3"
-
-      }]
-  },
   promo: {
     name: "Bem-builder",
     text: "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor " +
     "mauris condimentum nibh, ut fermentum massa justo sit amet.",
     link: "Попробывать",
     href: "#"
-  },
-  footer: {
-    copyright: 'Bem-builder'
   }
 };
 module.exports = data;
