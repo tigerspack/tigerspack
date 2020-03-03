d3e0c62dc08d32a0848531dd6f6b3bffa2f3c928
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2020-03-03 22:26:58 +0300

    #14: Button component

---
+++ src/Button/Button.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Button from './Button';
+
+const clickFn = jest.fn();
+
+describe('Button', () => {
+  it('should render correctly', () => {
+    const component = shallow(<Button onClick={clickFn}>Test button</Button>);
+    expect(component).toMatchSnapshot();
+  });
+  it('should call clickFn on button click', () => {
+    const component = shallow(<Button onClick={clickFn}>Test button</Button>);
+    component.simulate('click');
+    expect(clickFn).toHaveBeenCalled();
+  });
+});
