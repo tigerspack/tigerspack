40851f9b71f5d612b7ea0b9c6b1bffe4250ab539
Andrei Arkhipov <andrei@awb.pw>
2020-03-05 18:31:18 +0300

    #29: Full color palette

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,22 +1,52 @@
 export const colors = {
   primary: {
-    color: '#0092e6',
+    color: '#3F88C5',
     text: '#fff',
-    hoverColor: '#00a1e6',
+    hoverColor: '#619DCF',
     hoverText: '#fff',
   },
+  yellow: {
+    color: '#FDE74C',
+    text: '#304355',
+    hoverColor: '#FDEF8D',
+    hoverText: '#596977',
+  },
   danger: {
-    color: '#db3933',
+    color: '#E55934',
     text: '#fff',
-    hoverColor: '#e04245',
+    hoverColor: '#EC866B',
+    hoverText: '#fff',
+  },
+  warning: {
+    color: '#FA7921',
+    text: '#fff',
+    hoverColor: '#FB9D5D',
     hoverText: '#fff',
   },
   success: {
-    color: '#43cc58',
+    color: '#9BC53D',
     text: '#fff',
-    hoverColor: '#57c766',
+    hoverColor: '#B6D471',
     hoverText: '#fff',
   },
+  info: {
+    color: '#5BC0EB',
+    text: '#fff',
+    hoverColor: '#87D1F0',
+    hoverText: '#fff',
+  },
+  dark: {
+    color: '#304355',
+    text: '#fff',
+    hoverColor: '#596977',
+    hoverText: '#fff',
+  },
+  light: {
+    color: '#E5E8EC',
+    text: '#304355',
+    hoverColor: '#EEF0F2',
+    hoverText: '#596977',
+  }
 };
 
 export const colorsArray = ['primary', 'danger', 'success'];
