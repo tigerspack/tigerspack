a3c801609459e9520e94867f2beabc02c231a916
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2020-03-05 11:50:49 +0300

    #22: aphrodite was used

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
-const ButtonStyle = {
-  borderRadius: '4px',
-  boxSizing: 'border-box',
-  position: 'relative',
-  fontSize: '14px',
-  outline: 'none',
-  transition: 'all 0.1s',
-  display: 'inline-block',
-  marginBottom: 0,
-  fontWeight: 500,
-  textAlign: 'center',
-  verticalAlign: 'middle',
-  cursor: 'pointer',
-  backgroundImage: 'none',
-  border: '1px solid transparent',
-  whiteSpace: 'nowrap',
-  padding: '6px 16px',
-  lineHeight: 1.52857143,
-  userSelect: 'none',
-  backgroundColor: 'rgba(61, 94, 97, .9)',
-  color: '#fff',
-  ':hover': {
-    textDecoration: 'none',
-    backgroundColor: 'rgba(61, 94, 97, .8)',
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: '4px',
+    boxSizing: 'border-box',
+    position: 'relative',
+    fontSize: '14px',
+    outline: 'none',
+    transition: 'all 0.1s',
+    display: 'inline-block',
+    marginBottom: 0,
+    fontWeight: 500,
+    textAlign: 'center',
+    verticalAlign: 'middle',
+    cursor: 'pointer',
+    backgroundImage: 'none',
+    border: '1px solid transparent',
+    whiteSpace: 'nowrap',
+    padding: '6px 16px',
+    lineHeight: 1.52857143,
+    userSelect: 'none',
+    backgroundColor: 'rgba(61, 94, 97, .9)',
+    color: '#fff',
+    ':hover': {
+      textDecoration: 'none',
+      backgroundColor: 'rgba(61, 94, 97, .8)',
+    },
   },
-};
+});
 
 const Button = (props) => {
   const { children, onClick } = props;
   return (
-    <div style={ButtonStyle} onClick={onClick}>
+    <div className={css(styles.button)} onClick={onClick}>
       {children}
     </div>
   );
