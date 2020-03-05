a59958755b47e4dfe939b8202c46df26ceb7c4c0
Andrei Arkhipov <me@awb.pw>
2020-03-05 23:08:03 +0300

    #34: ability to change the color scheme was added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import ButtonComponent from './components/Button';
-
+import { setColor } from './utils/colors';
+// Components
 export const Button = ButtonComponent;
-
+// Method
+export const setColorScheme = setColor;
+// Default export
 export default null;
