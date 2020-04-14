e7c2498acd0bfdd614c71abbf5630255e480e630
Andrei Arkhipov <andrei@awb.pw>
2020-04-14 14:35:38 +0300

    #81: Badges were added to README.md

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import AlertComponent from './components/Alert';
 import ButtonComponent from './components/Button';
 import { setColor } from './utils/colors';
+
 // Components
 export const Alert = AlertComponent;
 export const Button = ButtonComponent;
+
 // Method
 export const setColorScheme = setColor;
+
 // Default export
 export default null;
