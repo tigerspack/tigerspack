9b6459637c786b6915aa494df49abb1566b1f5e0
Andrei Arkhipov <andrei@awb.pw>
2020-04-15 13:56:19 +0300

    #28: Test for Card component

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Card from './Alert';
+import Card from './Card';
 
 describe('Alert', () => {
   it('Default Alert', () => {
     const component = shallow(<Card>Test alert</Card>);
     expect(component).toMatchSnapshot();
-  });
-  it('Outline Alert', () => {
-    const component = shallow(<Card outline theme={'danger'}>Test alert</Card>);
-    expect(component).toMatchSnapshot();
-  });
-  it('Dismissible Alert', () => {
-    const component = shallow(<Card dismissible theme={'success'}>Test alert</Card>);
-    expect(component).toMatchSnapshot();
-  });
-  it('Dismissible Alert close function', () => {
-    const component = shallow(<Card dismissible>Test alert</Card>);
-    component.find(`.${component.get(0).props.children[0].props.className}`).simulate('click');
-    expect(component.get(0).props.style.display).toEqual('none');
   });
 });
