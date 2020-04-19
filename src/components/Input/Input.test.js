cd192661fd3460ce7805caaa705e5c35286e4fd4
Andrei Arkhipov <andrei@awb.pw>
2020-04-19 16:30:18 +0300

    #110: Bug witn Input component was fixed

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -18,18 +18,18 @@ describe('Input', () => {
   it('Input on focus', () => {
     const component = shallow(<Input placeholder={'test'}/>);
     component.childAt(1).simulate('focus');
-    expect(component.childAt(0).get(0).props.css.background).toEqual('transparent');
+    expect(component.childAt(0).get(0).props.css.background).toEqual('#fff');
   });
   it('Input on blur', () => {
     const component = shallow(<Input placeholder={'test'}/>);
     component.childAt(1).simulate('focus');
     component.childAt(1).simulate('blur');
-    expect(component.childAt(0).get(0).props.css.background).toEqual('#fff');
+    expect(component.childAt(0).get(0).props.css.background).toEqual('transparent');
   });
   it('Input on change', () => {
     const component = shallow(<Input placeholder={'test'}/>);
     const event = { target: { name: 'value', value: 'test' } };
     component.childAt(1).simulate('change', event);
-    expect(component.childAt(0).get(0).props.css.background).toEqual('transparent');
+    expect(component.childAt(0).get(0).props.css.background).toEqual('#fff');
   });
 });
