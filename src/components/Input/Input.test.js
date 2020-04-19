4eefffabfd09d9caf8613d65d6fa78abd4f69fb0
Andrei Arkhipov <andrei@awb.pw>
2020-04-19 15:50:58 +0300

    #108: Input component was covered with tests

--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -4,7 +4,32 @@ import Input from './Input';
 
 describe('Input', () => {
   it('Input default', () => {
-    const component = shallow(<Input placeholder={'Test'} />);
+    const component = shallow(<Input name={'test'} id={'test'} placeholder={'Test'} onKeyUp={() => {}} />);
     expect(component).toMatchSnapshot();
+  });
+  it('Input with error', () => {
+    const component = shallow(<Input error value={'error value'} />);
+    expect(component).toMatchSnapshot();
+  });
+  it('Input with valid value', () => {
+    const component = shallow(<Input valid value={'valid value'} />);
+    expect(component).toMatchSnapshot();
+  });
+  it('Input on focus', () => {
+    const component = shallow(<Input placeholder={'test'}/>);
+    component.childAt(1).simulate('focus');
+    expect(component.childAt(0).get(0).props.css.background).toEqual('transparent');
+  });
+  it('Input on blur', () => {
+    const component = shallow(<Input placeholder={'test'}/>);
+    component.childAt(1).simulate('focus');
+    component.childAt(1).simulate('blur');
+    expect(component.childAt(0).get(0).props.css.background).toEqual('#fff');
+  });
+  it('Input on change', () => {
+    const component = shallow(<Input placeholder={'test'}/>);
+    const event = { target: { name: 'value', value: 'test' } };
+    component.childAt(1).simulate('change', event);
+    expect(component.childAt(0).get(0).props.css.background).toEqual('transparent');
   });
 });
